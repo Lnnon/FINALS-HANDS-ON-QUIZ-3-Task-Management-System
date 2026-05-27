9e2d4123f92cfe6c2992bddae8d763fd6492e56f
Lennon Jarabese <123962015+Lnnon@users.noreply.github.com>
2026-05-28 04:19:34 +0800

    Update API URL to remove 'tasks' endpoint

--- frontend/src/services/api.js
+++ frontend/src/services/api.js
@@ -1,4 +1,4 @@
-const API_URL = 'https://lennonjarabese.pythonanywhere.com/api/tasks/';
+const API_URL = 'https://lennonjarabese.pythonanywhere.com/api/';
 
 export const getTasks = async () => {
   const res = await fetch(API_URL);
